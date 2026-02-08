1a87f6caf02cde27925f592bfaba72cc49c1703b
Guru Saran M <gurusaranm0025@gmail.com>
2026-02-08 19:40:12 +0530

    monitor daemon, battery monitor, version flags

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,20 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 	"github.com/wizarki972/myone/internal/modules/audio"
+	"github.com/wizarki972/myone/internal/modules/battery"
 	"github.com/wizarki972/myone/internal/modules/display"
 	"github.com/wizarki972/myone/internal/modules/screenshot"
 )
 
+const VERSION = "0.5.5-alpha"
+
 var brightness string
-var vol_notify, screen_shot bool
+var vol_notify, screen_shot, monitor_daemon, batt_mon, version bool
 
 var rootCMD = &cobra.Command{
 	Use:   "myone",
@@ -28,16 +32,34 @@ var rootCMD = &cobra.Command{
 			screenshot.OpenGUI()
 		}
 
+		if batt_mon {
+			battery.NewBatteryMonitor().StartMonitor()
+		}
+
+		if monitor_daemon {
+			display.NewMonitorDaemon().StartDaemon()
+		}
+
+		if version {
+			fmt.Println(VERSION)
+		}
+
 		return nil
 	},
 }
 
 func initializeFlags() {
-	rootCMD.Flags().StringVar(&brightness, "b", "", "+5% - increases the brightness by 5%, \n-5% decreases the brightness by 5%")
+	rootCMD.Flags().StringVar(&brightness, "bright", "", "+5% - increases the brightness by 5%, \n-5% decreases the brightness by 5%")
 
 	rootCMD.Flags().BoolVar(&vol_notify, "volume-osd", false, "just tells swayosd to show current volume level of the current sink.")
 
 	rootCMD.Flags().BoolVar(&screen_shot, "screenshot", false, "opens flameshot gui with the XDG_USER_DIR/Screenshot as the save path")
+
+	rootCMD.Flags().BoolVar(&batt_mon, "battery-monitor", false, "continously checks battery level and notifies the user when its lower")
+
+	rootCMD.Flags().BoolVar(&monitor_daemon, "monitor-daemon", false, "continuosly checks for new/removed monitors and changes the brightness based on the focused monitor.\n NOTE: does not support OLED or LED displays. Only supports LCD displays (displays with backlight)")
+
+	rootCMD.Flags().BoolVarP(&version, "version", "v", false, "prints the package version")
 }
 
 func Execute() {
